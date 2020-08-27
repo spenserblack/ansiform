1ca4ae46914b2bd7f706a168afe53e8ecfc68849
Spenser Black <spenserblack01@gmail.com>
2020-08-27 09:17:32 -0400

    Fail when first argument not literal

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,22 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#[macro_use]
+extern crate proc_macro;
+
+#[macro_use]
+extern crate quote;
+
+use proc_macro::{
+    TokenStream,
+    TokenTree::*,
+};
+
+#[proc_macro]
+pub fn color_format(tokens: TokenStream) -> TokenStream {
+    let mut tokens = tokens.into_iter();
+    let format_str = match tokens.next() {
+        None => return TokenStream::from( quote! { format!() }),
+        Some(Literal(literal)) => literal,
+        _ => panic!("First argument must be a literal"),
+    };
+
+    TokenStream::new()
 }
