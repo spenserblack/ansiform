d08b4852df54949965dff108f79a859498babd17
Spenser Black <spenserblack01@gmail.com>
2020-10-16 09:31:38 -0400

    Actually remove extra doc newline
    :facepalm:

--- src/lib.rs
+++ src/lib.rs
@@ -18,9 +18,9 @@ use regex::Regex;
 /// Adds ANSI escape codes to a formatting string, allowing ANSI colors to be set at compile time instead of runtime.
 ///
 /// # Example
+///
 /// ```rust
 /// # use ansiform::ansi;
-///
 /// println!(ansi!("It {;green,bold}!"), "works");
 /// println!(ansi!("{:#?;black,on bright white}!"), "Notes");
 /// println!(ansi!("Warning: {:#?;yellow}!"), Some("warning"));
